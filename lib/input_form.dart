fbc87fb23c9c3a92aee7120ff6e3670e4bc7af26
Naisinn <62951486+Naisinn@users.noreply.github.com>
2024-08-30 12:24:58 +0900

    gooひらがなAPI実装。ログに変換結果出力

--- lib/input_form.dart
+++ lib/input_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import "package:flutter_jissenn_08/data.dart";
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 // 入力状態のウィジェットを実装する
 class InputForm extends StatefulWidget {
@@ -11,6 +14,7 @@ class InputForm extends StatefulWidget {
 class _InputFormState extends State<InputForm> {
 
   final _formKey = GlobalKey<FormState>();
+  final _textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +27,7 @@ class _InputFormState extends State<InputForm> {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: TextFormField(
+              controller: _textEditingController,
               maxLines: 5,
               decoration: const InputDecoration(
                 hintText: '文章を入力してください',
@@ -38,9 +43,40 @@ class _InputFormState extends State<InputForm> {
           /* ◆ SizedBox サイズを指定できるWidget */
           const SizedBox(height: 20),
           ElevatedButton(
-            onPressed: () {
-              final formState = _formKey.currentState!;
-              formState.validate();
+            onPressed: () async {
+              final formState = _formKey.currentState;
+              if (formState == null || !formState.validate()) {
+                return;
+              }
+
+              final url = Uri.parse('https://labs.goo.ne.jp/api/hiragana');
+              final headers = {'Content-Type': 'application/json'};
+
+              final request = Request(
+                appId: const String.fromEnvironment('appId'),
+                sentence: _textEditingController.text,
+              );
+
+              try {
+                final result = await http.post(
+                  url,
+                  headers: headers,
+                  body: jsonEncode(request.toJson()),
+                );
+
+                if (result.statusCode == 200) {
+                  final response = Response.fromJson(
+                    jsonDecode(result.body) as Map<String, dynamic>,
+                  );
+                  debugPrint('変換結果: ${response.converted}');
+                } else {
+                  // エラーハンドリング
+                  debugPrint('APIリクエストエラー: ${result.statusCode}');
+                }
+              } catch (e) {
+                // エラーハンドリング
+                debugPrint('APIリクエストエラー: $e');
+              }
             },
             child: const Text(
               '変換',
@@ -49,5 +85,10 @@ class _InputFormState extends State<InputForm> {
         ],
       )
     );
+  }
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
   }
 }
